1a5e98b6cf185073efef18440af292e958f6cf05
Ralph Göstenmeier <rg@via-internet.de>
2018-07-29 16:53:12 +0200

    V1

--- 11/src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EmailValidatorDirective } from './email-validator/email-validator';
-@NgModule({
-	declarations: [EmailValidatorDirective],
-	imports: [],
-	exports: [EmailValidatorDirective]
-})
-export class DirectivesModule {}
